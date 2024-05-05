4e28676d34b9197d85783c5c6a9dfe61c04b8304
dev <109991303+devjetz@users.noreply.github.com>
2024-05-05 21:49:35 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.11'
+DESCRIPTION = 'Top quality printing.'
+LONG_DESCRIPTION = 'A package that let you print whatever you want, with more debbuging features and colors.'
+
+# Setting up
+setup(
+    name="Brint",
+    version=VERSION,
+    author="@dogsheet",
+    author_email="<commandobussiness@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
